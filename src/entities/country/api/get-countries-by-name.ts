1318b002784cd5d26a1ac5258b82fde1a6aac66b
semlex <picasso.semenov.spb@gmail.com>
2025-06-04 05:02:47 +0300

    smlrefactor

--- src/entities/country/api/get-countries-by-name.ts
+++ src/entities/country/api/get-countries-by-name.ts
@@ -3,12 +3,14 @@ import { CountryInfo } from '../model/types';
 import countries from '../model/countries.json';
 
 export async function getCountriesByName(countryName: string): Promise<CountryInfo[]> {
-  await new Promise((resolve) => {
-    setTimeout(resolve, getRandom(100, 800));
-  });
   if (typeof countryName !== 'string' || !countryName) {
     return [];
   }
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, getRandom(100, 800));
+  });
+
   const searchText = countryName.toLocaleLowerCase();
   return countries.filter(
     (x) =>
